d3971f67920b35965b3c3003eb26aa2881f7fc93
Alexandre Sullivan Teixeira <ast.sullivan@gmail.com>
2021-11-29 13:49:58 -0300

    exercicios dia 01

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicio02.js
@@ -0,0 +1,11 @@
+let valor1 = 5;
+let valor2 = 10;
+
+console.log(valor1 + valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+console.log(valor1 / valor2);
+console.log(valor1 % valor2);
+
+
+
